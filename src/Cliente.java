d265a5cb8b30d2a739369d3bfcea6d458ff15789
RafaelPinheiroCosta <rafaelcost@gmail.com>
2023-07-15 14:31:05 -0300

    Mudança nos metodos para retornar informações mais claras ao cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -10,12 +10,19 @@ public class Cliente {
 
     public void depositar(float valorDepositado) {
         this.saldo = valorDepositado;
+        System.out.println("Você depositou R$"+valorDepositado+"na conta");
+        System.out.println("O saldo do cliente "+this.nome+" agora é de R$"+this.saldo);
     }
 
     public boolean sacar(float valorQueSeraSacado) {
 
         if (valorQueSeraSacado < this.saldo) {
             this.saldo -= valorQueSeraSacado;
+
+            System.out.println("Você sacou R$"+valorQueSeraSacado+"da conta");
+            System.out.println("O saldo do cliente "+this.nome+" agora é de R$"+this.saldo);
+
+
             return true;
         } else {
             System.out.println("Saldo insuficiente para fazer essa transação");
@@ -28,6 +35,11 @@ public class Cliente {
 
         if (sacar(valorQueSeraTransferido)) {
             cliente.depositar(valorQueSeraTransferido);
+
+            System.out.println("Você esta transferindo R$"+valorQueSeraTransferido+"da conta do(a) cliente "
+                                    +this.nome+" para o(a) cliente "+cliente.nome);
+            System.out.println("O saldo do cliente "+this.nome+" agora é de R$"+this.saldo);
+            System.out.println("O saldo do cliente "+cliente.nome+" agora é de R$"+cliente.saldo);
         }
     }
 
